4508523f58e632155f2e7a49e46a4037695a4649
Miguel Figuera <miguelqui725@gmail.com>
2022-05-18 19:45:04 -0400

    text module

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require_relative "text_module.rb"
+
 # player creation
 class Player 
     def initialize(name,rol)
@@ -9,11 +11,33 @@ end
 #board display and interaction
 
 class Board 
-    attr_reader :dot
+    attr_reader :dot, :turn
     def initialize 
         @dot=[["o","o","o","o"],["o","o","o","o"],["o","o","o","o"],["o","o","o","o"],
         ["o","o","o","o"],["o","o","o","o"],["o","o","o","o"],["o","o","o","o"],
         ["o","o","o","o"],["o","o","o","o"],]
+        @turn=0
+    end
+
+    def view
+        puts <<-HEREDOC
+        #{dot[0][0]}|#{dot[0][1]}|#{dot[0][2]}|#{dot[0][3]}|
+        #{dot[1][0]}|#{dot[1][1]}|#{dot[1][2]}|#{dot[1][3]}|
+        #{dot[2][0]}|#{dot[2][1]}|#{dot[2][2]}|#{dot[2][3]}|
+        #{dot[3][0]}|#{dot[3][1]}|#{dot[3][2]}|#{dot[3][3]}|
+        #{dot[4][0]}|#{dot[4][1]}|#{dot[4][2]}|#{dot[4][3]}|
+        #{dot[5][0]}|#{dot[5][1]}|#{dot[5][2]}|#{dot[5][3]}|
+        #{dot[6][0]}|#{dot[6][1]}|#{dot[6][2]}|#{dot[6][3]}|
+        #{dot[7][0]}|#{dot[7][1]}|#{dot[7][2]}|#{dot[7][3]}|
+        #{dot[8][0]}|#{dot[8][1]}|#{dot[8][2]}|#{dot[8][3]}|
+        #{dot[9][0]}|#{dot[9][1]}|#{dot[9][2]}|#{dot[9][3]}|
+        HEREDOC
+    end
+
+    def update
+        input=gets.chomp.split("")
+        dot[@turn].replace(array)
+        @turn += 1
     end
 end
 
---
+++ text_module.rb
@@ -0,0 +1,28 @@
+#frozen_string_literals: true
+module Text 
+    #Welcome, rules and explanations
+    def the_welcome_text
+        puts "Welcome, lets play MasterMind on the console, shall we?"
+    end
+
+    def colors_text
+        puts "The colors of each numbers are:\n 
+        1 = \n
+        2 = \n
+        3 = \n
+        4 = \n
+        5 = \n
+        6 = \n
+        "
+    end
+
+    def rules_text
+
+    def select_your_rol_text
+        puts "What do you want to be? MasterMind (M) or CodeBreaker(C)?"   
+    end
+
+    #methods text
+    def update_the_board_text
+        puts "Choose your colors"
+    end
